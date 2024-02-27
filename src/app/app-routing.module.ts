9ae81d3964a74b7be72b1398165abdfd1d03ab54
Tseyram <eyram.tsekpuia@gmail.com>
2024-02-27 19:30:46 +0100

    with security

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountsComponent } from './accounts/accounts.component';
 import { CustomersComponent } from './customers/customers.component';
 import { NewCustomerComponent } from './new-customer/new-customer.component';
+import {LoginComponent} from "./login/login.component";
+import {AdminTemplateComponent} from "./admin-template/admin-template.component";
+import {NotAuthorizedComponent} from "./not-authorized/not-authorized.component";
+import {authorizationGuard} from "./guards/authorization.guard";
 
 const routes: Routes = [
-  {path:"customers", component: CustomersComponent},
-  {path: "accounts", component: AccountsComponent},
-  {path:"new-customer", component:NewCustomerComponent},
+
+  {path:"",redirectTo:"/login",pathMatch:"full"},
+  {path:"login",component:LoginComponent},
+  {path:"admin",component:AdminTemplateComponent, children:[{path:"customers", component: CustomersComponent},
+      {path: "accounts", component: AccountsComponent},
+      {path:"new-customer", component:NewCustomerComponent, canActivate :[authorizationGuard], data:{role:"ADMIN"}},]},
+  {path:"notAuthorized", component:NotAuthorizedComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AccountsComponent } from './accounts/accounts.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,11 @@ import { AppComponent } from './app.component';
 import { CustomersComponent } from './customers/customers.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { LoginComponent } from './login/login.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import {AppHttpInterceptor} from "./inteceptors/app-http.interceptor";
+import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
+import {authorizationGuard} from "./guards/authorization.guard";
 
 @NgModule({
   declarations: [
@@ -16,7 +21,10 @@ import { NewCustomerComponent } from './new-customer/new-customer.component';
     NavbarComponent,
     CustomersComponent,
     AccountsComponent,
-    NewCustomerComponent
+    NewCustomerComponent,
+    LoginComponent,
+    AdminTemplateComponent,
+    NotAuthorizedComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +33,8 @@ import { NewCustomerComponent } from './new-customer/new-customer.component';
     ReactiveFormsModule
   ],
   providers: [
+    authorizationGuard,
+    {provide :HTTP_INTERCEPTORS, useClass : AppHttpInterceptor, multi:true},
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
